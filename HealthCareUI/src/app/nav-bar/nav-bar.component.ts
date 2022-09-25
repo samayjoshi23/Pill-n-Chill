e6932cea02300b244c98d5723a77e13184a39b77
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-09-25 15:12:13 +0530

    Improved Navbar Functionality

--- HealthCareUI/src/app/nav-bar/nav-bar.component.ts
+++ HealthCareUI/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,7 +9,18 @@ export class NavBarComponent implements OnInit {
 
   constructor() { }
 
+  navChange : boolean = false;
+  nav = document.querySelector('#nav') as HTMLDivElement;
+
   ngOnInit(): void {
+  }
+  @HostListener("document:scroll")
+  scrollFunction(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+      this.navChange = true;
+    }
+    else
+      this.navChange = false;
   }
 
 }
