2f1548f185e86848a2ae1c89940f1e5ff5f44354
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-09-27 00:40:10 +0530

    improved UI for product list page

---
+++ HealthCareUI/src/app/Models/MedicideModel.ts
@@ -0,0 +1,7 @@
+export interface Medicine{
+    medId: string,
+    name: string,
+    price: number,
+    mrp: number,
+    seller: string,
+}
--- HealthCareUI/src/app/app-routing.module.ts
+++ HealthCareUI/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLandingPageComponent } from './Admin/admin-landing-page/admin-landing-page.component';
 import { LandingPageComponent } from './Client/landing-page/landing-page.component';
+import { ListPageComponent } from './Client/list-page/list-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
   },
-
+  {
+    path: 'products',
+    component: ListPageComponent,
+  },
 
   // Admin Paths
   {
