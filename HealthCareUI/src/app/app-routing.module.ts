3b768c15ba72ba47d5f337c71adb0815e6513de4
abhiityagi <tyagiabhiin11@gmail.com>
2022-09-27 14:27:49 +0530

    some changes :fire:

--- HealthCareUI/src/app/app-routing.module.ts
+++ HealthCareUI/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLandingPageComponent } from './Admin/admin-landing-page/admin-landing-page.component';
+import { DashBoardComponent } from './Admin/dash-board/dash-board.component';
 import { LandingPageComponent } from './Client/landing-page/landing-page.component';
 
 const routes: Routes = [
@@ -14,7 +15,15 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminLandingPageComponent,
+    // children: [{
+    //   path: 'dashboard',
+    //   component: DashBoardComponent
+    // }]
   },
+  {
+    path: 'admin/dashboard',
+    component: DashBoardComponent
+  }
 ];
 
 @NgModule({
