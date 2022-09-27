a9fb054fd3643ebd57119f61fbce79a188cf85c0
Nikhil-Kemnie <nikhil26kemnie@gmail.com>
2022-09-27 14:46:23 +0530

    commiting

---
+++ HealthCareAPI/HealthCareAPI/Models/demoUser.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthCareAPI.Models
+{
+    public class demoUser
+    {
+        public string Username { get; set; }
+        public string role { get; set; }
+        public int age { get; set; }
+    }
+}
