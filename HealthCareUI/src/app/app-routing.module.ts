7996982b6ce7419fd1771801e5a572c3ede13bb9
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-09-28 13:42:38 +0530

    heavy UI changes in admin panel

--- HealthCareUI/src/app/app-routing.module.ts
+++ HealthCareUI/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLandingPageComponent } from './Admin/admin-landing-page/admin-landing-page.component';
+import { AddCategoryComponent } from './Admin/dash-board/Categories/add-category/add-category.component';
+import { CategoryListComponent } from './Admin/dash-board/Categories/category-list/category-list.component';
+import { EditCategoryComponent } from './Admin/dash-board/Categories/edit-category/edit-category.component';
 import { DashBoardComponent } from './Admin/dash-board/dash-board.component';
+import { OrderListComponent } from './Admin/dash-board/Orders/order-list/order-list.component';
+import { AddProductComponent } from './Admin/dash-board/Products/add-product/add-product.component';
+import { EditProductComponent } from './Admin/dash-board/Products/edit-product/edit-product.component';
+import { ProductListComponent } from './Admin/dash-board/Products/product-list/product-list.component';
+import { UserListComponent } from './Admin/dash-board/Users/user-list/user-list.component';
 import { LandingPageComponent } from './Client/landing-page/landing-page.component';
 import { ListPageComponent } from './Client/list-page/list-page.component';
 import { ProductPageComponent } from './Client/product-page/product-page.component';
+import { ErrorPageComponent } from './Utility/error-page/error-page.component';
 
 const routes: Routes = [
   {
@@ -18,16 +27,61 @@ const routes: Routes = [
 
   // Admin Paths
   {
+    title: 'PillnChill - Admin',
     path: 'admin',
-    component: AdminLandingPageComponent,
-    // children: [{
-    //   path: 'dashboard',
-    //   component: DashBoardComponent
-    // }]
+    component: DashBoardComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: AdminLandingPageComponent,
+      },
+      {
+        path:'users',
+        component: UserListComponent
+      },
+      {
+        path:'categories',
+        component: CategoryListComponent
+      },
+      {
+        path:'categories',
+        children: [
+          {
+            path: 'add',
+            component: AddCategoryComponent
+          },
+          {
+            path: ':id',
+            component: EditCategoryComponent
+          }
+        ]
+      },
+      {
+        path:'orders',
+        component: OrderListComponent
+      },
+      {
+        path:'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'products',
+        children: [
+          {
+            path: 'add',
+            component: AddProductComponent
+          },
+          {
+            path: ':id',
+            component: EditProductComponent
+          }
+        ]
+      }
+    ],
   },
   {
-    path: 'admin/dashboard',
-    component: DashBoardComponent
+    path: '**',
+    component: ErrorPageComponent
   }
 ];
 
