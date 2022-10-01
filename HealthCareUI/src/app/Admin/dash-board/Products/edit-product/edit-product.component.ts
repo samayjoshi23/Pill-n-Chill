c2f99c6044765a44e57726b792ae89d724268925
abhiityagi <tyagiabhiin11@gmail.com>
2022-10-02 01:22:20 +0530

    minor changes :wrench:

--- HealthCareUI/src/app/Admin/dash-board/Products/edit-product/edit-product.component.ts
+++ HealthCareUI/src/app/Admin/dash-board/Products/edit-product/edit-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
+  addProductForm : FormGroup = new FormGroup({
+    medicineName: new FormControl('', [Validators.required, Validators.minLength(3)]),  
+    chemicalName: new FormControl('', [Validators.required, Validators.minLength(3)]),  
+    brandName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    photo: new FormControl('', [Validators.required]),  
+    price: new FormControl('', [Validators.required, Validators.pattern("^[1-9]\d{0,7}(?:\.\d{1,4})?|\.\d{1,4}$")]),  
+    sellerName: new FormControl('', [Validators.required, Validators.minLength(3)])  
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  editProduct(){
+    console.log(this.addProductForm);
   }
 
 }
